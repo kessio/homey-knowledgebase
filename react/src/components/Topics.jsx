34c38bf628abd1e211ab7988cf8981632dff8a14
kessio <sharonkessio6@gmail.com>
2024-01-04 14:16:59 +0300

    Topics section

--- react/src/components/Topics.jsx
+++ react/src/components/Topics.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BiBuildingHouse, BiHomeHeart, BiUser } from 'react-icons/bi';
+
 
 function Topics() {
   return (
-    <div>Topics</div>
+    <section className="bg-rose-600 py-24">
+        <div className="flex flex-col gap-6 sm:flex-row gap-6 justify-center">
+      <div className="flex flex-col items-center mr-16">
+        <BiHomeHeart size={70} color="white" />
+        <p className="mt-2 text-lg text-white font-bold">For Renters</p>
+      </div>
+      <div className="flex flex-col items-center mr-16">
+        <BiBuildingHouse size={70} color="white" />
+        <p className="mt-2 text-lg text-white font-bold ">For Landlords</p>
+      </div>
+      <div className="flex flex-col items-center mr-16">
+        <BiUser size={70} color="white" />
+        <p className="mt-2 text-lg text-white font-bold">For Agents</p>
+      </div>
+    </div>
+    </section>
+    
   )
 }
 
