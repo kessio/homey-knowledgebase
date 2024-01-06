e135a9c28d18ce1da57acf795ac8b661ed44ce82
kessio <sharonkessio6@gmail.com>
2024-01-06 20:28:15 +0300

    edit script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,10 +8,12 @@ npm run build
 
 cd ..
 bundle install
+./bin/rails db:migrate
+./bin/rails db:seed
+
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 
-./bin/rails db:migrate
-./bin/rails db:seed
+
 
 
