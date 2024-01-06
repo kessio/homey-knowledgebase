6b0c15afeb7dfba6f6b97eb597ed6e654526c682
kessio <sharonkessio6@gmail.com>
2024-01-06 20:23:02 +0300

    edit build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,16 @@
 # exit on error
 set -o errexit
 
-bundle install
+cd react
 npm install
 npm run build  
 
+cd ..
+bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 
 ./bin/rails db:migrate
 ./bin/rails db:seed
+
+
