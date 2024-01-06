bbe8850adb5e33b4179ffeba6cbed26c3b24661c
kessio <sharonkessio6@gmail.com>
2024-01-06 09:13:49 +0300

    question controller tests

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,18 +1,4 @@
 class Api::V1::QuestionsController < ApplicationController
-  before_action :set_question, only: %i[ show update destroy ]
-
-  # GET /questions
-  def index
-    @questions = Question.all
-
-    render json: @questions, status: :ok
-  end
-
-  # GET /questions/1
-  def show
-    render json: @question
-  end
-
   # POST /questions
   def create
     search_term = params[:question][:search_term].downcase.strip
@@ -22,30 +8,11 @@ class Api::V1::QuestionsController < ApplicationController
     if @question.save
       render json: @question, status: :created
     else
-      render json: @question.errors, status: :unprocessable_entity
+      render json: { errors: @question.errors.full_messages }, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /questions/1
-  def update
-    if @question.update(question_params)
-      render json: @question
-    else
-      render json: @question.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /questions/1
-  def destroy
-    @question.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_question
-      @question = Question.find(params[:id])
-    end
-
     def question_params
       params.require(:question).permit(:email, :search_term) 
     end
