311de243c8cbb16a4c8d3a4f738a39cf401f7dc8
kessio <sharonkessio6@gmail.com>
2024-01-07 22:19:38 +0300

    loading skeleton for recent and popular searches

--- react/src/components/Analytics.jsx
+++ react/src/components/Analytics.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Skeleton from 'react-loading-skeleton';
+import LoadingSkeletonContent from './LoadingSkeletonContent';
 
 export default function Analytics() {
   const apiUrl = import.meta.env.VITE_API_URL;
   const [allRecent, setAllRecent] = useState([])
   const [allPopular, setPopular] = useState([])
+  const [loadingRecent, setLoadingRecent] = useState(true);
+  const [loadingPopular, setLoadingPopular] = useState(true);
 
   const fetchRecent = async() => {
     try {
@@ -18,7 +22,9 @@ export default function Analytics() {
       setAllRecent(res.data);
     } catch (err) {
       console.log("Error Fetching Recent",err);
-    } 
+    } finally {
+      setLoadingRecent(false); 
+    }
 
   }
 
@@ -33,30 +39,46 @@ export default function Analytics() {
       setPopular(res.data);
     } catch (err) {
       console.log("Error Fetching Popular",err);
-    } 
+    } finally {
+      setLoadingPopular(false); 
+    }
   }
 
   useEffect(() => {
-    fetchPopular();
-    fetchRecent();
+    Promise.all([fetchRecent(), fetchPopular()]).then(() => {
+      
+    });
   },[]);
   return (
     <section className="py-24">
         <div className="flex flex-col pl-10 md:flex-row gap-14 justify-center mx-auto max-w-screen-lg">
-            <ul className="w-1/2">
-                <h2 className="font-bold">Recent Searches</h2>
-                {Array.isArray(allRecent) && allRecent.map((recent) => (
-                  <li key={recent.id} className="text-rose-500 list-disc py-2"><span className="text-gray-500 font-semibold">{recent.title}</span></li>
-                ))}
-            </ul>
-            <ul className="w-1/2">
-            <h2 className="font-bold">Popular Searches</h2>
-            {Array.isArray(allPopular) && allPopular.map((popular) => (
-                  <li key={popular.id} className=" text-rose-500 list-disc py-2"><span className="text-gray-500 font-semibold">{popular.title}</span></li>
-                )
-            )}
-                
-            </ul>
+        <ul className="w-1/2">
+          <h2 className="font-bold">Recent Searches</h2>
+          {loadingRecent ? (
+           <LoadingSkeletonContent/>
+           
+          ) : (
+            Array.isArray(allRecent) &&
+            allRecent.map((recent) => (
+              <li key={recent.id} className="text-rose-500 list-disc py-2">
+                <span className="text-gray-500 font-semibold">{recent.title}</span>
+              </li>
+            ))
+          )}
+        </ul>
+        <ul className="w-1/2">
+          <h2 className="font-bold">Popular Searches</h2>
+          {loadingPopular ? (
+            <LoadingSkeletonContent />
+          ) : (
+            Array.isArray(allPopular) &&
+            allPopular.map((popular) => (
+              <li key={popular.id} className=" text-rose-500 list-disc py-2">
+                <span className="text-gray-500 font-semibold">{popular.title}</span>
+              </li>
+            ))
+          )}
+        </ul>
         </div>
 
     </section>
---
+++ react/src/components/LoadingSkeletonContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Skeleton from 'react-loading-skeleton';
+
+function LoadingSkeletonContent() {
+  return (
+    <div className="mt-2">
+    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
+    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
+    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
+    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
+    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
+   </div>
+    
+  )
+}
+
+export default LoadingSkeletonContent
