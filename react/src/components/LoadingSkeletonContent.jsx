54011ef765e7e1fa81b86d95b15c0286b52526c2
kessio <sharonkessio6@gmail.com>
2024-01-08 01:16:15 +0300

    chnage skeleton

--- react/src/components/LoadingSkeletonContent.jsx
+++ react/src/components/LoadingSkeletonContent.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function LoadingSkeletonContent() {
   return (
     <div className="mt-2">
-    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
-    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
-    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
-    <div className="bg-gray-400 mb-2"> <Skeleton count={1} height={5} /></div>
+    <div className="mb-2"> <Skeleton count={5} height={20} showAnimation={true} color="#f4f5f7" highlightColor="#e5e5e5" /></div>
+    
    </div>
     
   )
