ca894b72c3cabcb9e3ac18c8d379af4233e1f65a
kessio <sharonkessio6@gmail.com>
2024-01-07 22:58:29 +0300

    loading

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "https://front-end-tvjy.onrender.com"
+     origins "*"  # https://front-end-tvjy.onrender.com
 
      resource "*",
        headers: :any,
